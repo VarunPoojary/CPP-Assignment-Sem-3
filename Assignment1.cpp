8137fb57ad785cc2c4360e54658d8e0cd2f485e9
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2021-09-21 19:39:44 +0530

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -1,261 +1,152 @@
- #include <iostream>
-    using namespace std;
-    int main() {
-  
-    int n1,m1,n2,m2; //Variable Declaration
-    cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    cin >>n1 >>m1;
-    cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    cin >>n2 >>m2;
-    int a[n1][m1], b[n2][m2] , ans[n1][m2]; //Declaration of 2d arrays
-    cout << "Enter the elements of first matrix :"<<endl; 
-    for (int i = 0; i < n1; i++) 
-    {
-        for (int j = 0; j <m1; j++) 
-        {
-            cin >> a[i][j];
-        }
-    }
+#include<iostream>
+using namespace std;
+#define s 20
+class matrix
+{
+  public:
+  int a[s][s],b[s][s],res[s][s],r1,c1,r2,c2,rr,cr;
+  void getMatrix();
+  void display();
+  void add_matrix();
+  void sub_matrix();
+  void multi_matrix();
+};
 
-    cout << "Enter the elements of second matrix :" <<endl;
-    for (int i = 0; i <n2; i++) 
+
+void matrix::getMatrix()
+{
+    cout<<"\nEnter number of rows and columns of matrix a:";
+    cin>>r1>>c1;
+    for(int i=0;i<r1;i++)
     {
-        for (int j = 0; j <m2; j++) 
+        for(int j=0;j<c1;j++)
         {
-            cin >> b[i][j];
+            cin>>a[i][j];
         }
     }
     
+    cout<<"\nEnter number of rows and columns of matrix b:";
+    cin>>r2>>c2;
+    for(int i=0;i<r2;i++)
+    {
+        for(int j=0;j<c2;j++)
+        {
+            cin>>b[i][j];
+        }
+    }
+    cout<<endl;
+}
+
+
+void matrix::display()
+{
+    cout<<"Resultant Matrix is: "<<endl;
+    for(int i=0;i<rr;i++)
+    {
+        for(int j=0;j<cr;j++)
+        {
+            cout<<res[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+}
+
+
+void matrix::add_matrix()
+{
+    rr=r1;
+    cr=c1;
+    if(r1==r2 && c1==c2)
+    {
+      for(int i=0;i<rr;i++)
+        {
+            for(int j=0;j<cr;j++)
+            {
+            res[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        display(); 
+    }
+    else
+    {
+        cout<<"Matrix Addition Not Posible"<<endl;
+    }
+    
+}
+
+
+
+void matrix::sub_matrix()
+{
+    rr=r1;
+    cr=c1;
+    if(r1==r2 && c1==c2)
+    {
+      for(int i=0;i<rr;i++)
+        {
+            for(int j=0;j<cr;j++)
+            {
+            res[i][j]=a[i][j]-b[i][j];
+            }
+        }
+        display(); 
+    }
+    else
+    {
+        cout<<"Matrix Subtraction Not Posible"<<endl;
+    }
+    
+}
+
+
+void matrix::multi_matrix()
+{
+    if(r1 != c2 && c1 != r2){
+        cout<<"\nMatrix Multiplication Not Possible!\n";
+        return;
+    }
+    else{
+        rr = r1;
+        cr = c2;
+        res[rr][cr];
+        for (int i = 0; i < r1; i++) 
+        {
+		    for (int j = 0; j < c2; j++) 
+		    {
+                res[i][j]=0;
+			    for (int k = 0; k<c1; k++) {
+				    res[i][j] += a[i][k] * b[k][j];
+			    }
+		    }
+	    }
+        display();
+    }
+}
+
+
+int main()
+{
+    matrix obj;
+    char n;
     cout<<"Press 1 for addition"<<endl<<"Press 2 for subtraction"<<endl<<"Press 3 for Multiplication"<<endl;
-    int c;
-    cin>>c;
-    switch(c)
+    cin>>n;
+    
+    switch(n)
     {
-        case 1:
-        cout<<"Sum of 2 Arrays:"<<endl;
-            for (int i = 0; i <n1; i++) 
-                {
-                    for (int j = 0; j <m1; j++) 
-                    {
-                        ans[i][j] = a[i][j] + b[i][j];
-                        
-                        cout<< ans[i][j] << "  ";
-                    }
-                    cout<<endl;
-                }
-            break;
-            
-        case 2:
-        cout<<"Difference of 2 Arrays:"<<endl;
-            for (int i = 0; i <n1; i++) 
-                {
-                    for (int j = 0; j <m1; j++) 
-                    {
-                        ans[i][j] = a[i][j] - b[i][j];
-                        
-                        cout<< ans[i][j] << "  ";
-                    }
-                    cout<<endl;
-                }
-            break;
-        
-        case 3:
-        cout<<"Multiplication of 2 Arrays"<<endl;
-        if(m1==n2)
-        {
-            for (int i = 0; i <n1; i++) 
-                {
-                    for (int j = 0; j <m2; j++) 
-                    {
-                        ans[i][j]=0;
-                        for(int k=0;k<m1;k++)
-                        {
-                            ans[i][j]+=a[i][k] * b[k][j];
-                        }
-                        
-                        
-                        cout<< ans[i][j] << "  ";
-                    }
-                    cout<<endl;
-                }
-            
-        }
-        else
-        {
-            cout<<"Enter Proper Parameters";
-        }
-            break;
-        
+        case '1':
+        obj.getMatrix();
+        obj.add_matrix();
+        break;
+        case '2':
+        obj.getMatrix();
+        obj.sub_matrix();
+        break;
+        case '3':
+        obj.getMatrix();
+        obj.multi_matrix();
+        break;
         default:
-            cout<<"Enter correct choice";
+        cout<<"Invalid Choice"<<endl;
         
     }
-
-    return 0;
-    }
-
-
-
-
-
-
-    // #include <iostream>
-    // using namespace std;
-    
-    // void addition()
-    // {
-    // int n1,m1,n2,m2;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n1 >>m1;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n2 >>m2;
-    // int a[n1][m1], b[n2][m2] , ans[n1][m2]; //Declaration of 2d arrays
-    // cout << "Enter the elements of first matrix :"<<endl; 
-    // for (int i = 0; i < n1; i++) 
-    // {
-    //     for (int j = 0; j <m1; j++) 
-    //     {
-    //         cin >> a[i][j];
-    //     }
-    // }
-
-    // cout << "Enter the elements of second matrix :" <<endl;
-    // for (int i = 0; i <n2; i++) 
-    // {
-    //     for (int j = 0; j <m2; j++) 
-    //     {
-    //         cin >> b[i][j];
-    //     }
-    // }
-    //     cout<<"Sum of 2 Arrays:"<<endl;
-    //         for (int i = 0; i <n1; i++) 
-    //             {
-    //                 for (int j = 0; j <m1; j++) 
-    //                 {
-    //                     ans[i][j] = a[i][j] + b[i][j];
-                        
-    //                     cout<< ans[i][j] << "  ";
-    //                 }
-    //                 cout<<endl;
-    //             }
-    // }
-    
-    
-    // void subtraction()
-    // {
-    // int n1,m1,n2,m2;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n1 >>m1;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n2 >>m2;
-    // int a[n1][m1], b[n2][m2] , ans[n1][m2]; //Declaration of 2d arrays
-    // cout << "Enter the elements of first matrix :"<<endl; 
-    // for (int i = 0; i < n1; i++) 
-    // {
-    //     for (int j = 0; j <m1; j++) 
-    //     {
-    //         cin >> a[i][j];
-    //     }
-    // }
-
-    // cout << "Enter the elements of second matrix :" <<endl;
-    // for (int i = 0; i <n2; i++) 
-    // {
-    //     for (int j = 0; j <m2; j++) 
-    //     {
-    //         cin >> b[i][j];
-    //     }
-    // }
-    //     cout<<"Difference of 2 Arrays:"<<endl;
-    //         for (int i = 0; i <n1; i++) 
-    //             {
-    //                 for (int j = 0; j <m1; j++) 
-    //                 {
-    //                     ans[i][j] = a[i][j] - b[i][j];
-                        
-    //                     cout<< ans[i][j] << "  ";
-    //                 }
-    //                 cout<<endl;
-    //             }
-    // }
-    
-    // void multiplication()
-    // {
-    // int n1,m1,n2,m2;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n1 >>m1;
-    // cout << "Enter the number of Rows and Columns of 1st matrix : ";
-    // cin >>n2 >>m2;
-    // int a[n1][m1], b[n2][m2] , ans[n1][m2]; //Declaration of 2d arrays
-    // cout << "Enter the elements of first matrix :"<<endl; 
-    // for (int i = 0; i < n1; i++) 
-    // {
-    //     for (int j = 0; j <m1; j++) 
-    //     {
-    //         cin >> a[i][j];
-    //     }
-    // }
-
-    // cout << "Enter the elements of second matrix :" <<endl;
-    // for (int i = 0; i <n2; i++) 
-    // {
-    //     for (int j = 0; j <m2; j++) 
-    //     {
-    //         cin >> b[i][j];
-    //     }
-    // }
-    
-    // cout<<"Multiplication of 2 Arrays"<<endl;
-    //     if(m1==n2)
-    //     {
-    //         for (int i = 0; i <n1; i++) 
-    //             {
-    //                 for (int j = 0; j <m2; j++) 
-    //                 {
-    //                     ans[i][j]=0;
-    //                     for(int k=0;k<m1;k++)
-    //                     {
-    //                         ans[i][j]+=a[i][k] * b[k][j];
-    //                     }
-                        
-                        
-    //                     cout<< ans[i][j] << "  ";
-    //                 }
-    //                 cout<<endl;
-    //             }
-            
-    //     }
-    //     else
-    //     {
-    //         cout<<"Enter Proper Parameters";
-    //     }
-        
-        
-        
-    // }
-
-    // int main() 
-    // {
-    // cout<<"Press 1 for addition"<<endl<<"Press 2 for subtraction"<<endl<<"Press 3 for Multiplication"<<endl;
-    // int c;
-    // cin>>c;
-    // switch(c)
-    // {
-    //     case 1:
-    //      addition();
-    //         break;
-            
-    //     case 2:
-    //         subtraction();
-    //         break;
-        
-    //     case 3:
-    //         multiplication();
-    //         break;
-        
-    //     default:
-    //         cout<<"Enter correct choice";
-        
-    // }
-    // return 0;
-    // }
+}
