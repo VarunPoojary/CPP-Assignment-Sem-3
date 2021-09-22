63c144ae310a1512b04a21be2645a8eb65ad655e
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2021-09-22 10:11:08 +0530

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,70 +1,98 @@
-#include<iostream>
-    using namespace std;
-    int main()
+
+/* 
+    A matrix of m x n order is said to have a saddle point; if some entry at a[i][j] is the smallest value in row i and the
+    largest value in j. Write C++ function that determines the location of a saddle point if it exists.
+*/
+
+#include <iostream>
+using namespace std;
+# define s 20
+
+class saddle
+{
+    public :
+    int a[s][s],r,c;
+    void get_matrix();
+    void saddle_point();
+};
+
+
+
+void saddle::get_matrix()
+{
+    cout<<"\nEnter Array Dimensions : ";
+    cin>>r>>c;
+    cout<<"\nEnter Array Elements "<<endl;
+    for (int i = 0; i < r; i++) 
     {
-        int r,c;
-        cout<<"Enter Number of Rows and Col ";
-        cin>>r>>c;
-        int a[r][c];
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c;j++)
-            {
-                cin>>a[i][j];
+		for (int j = 0; j < c; j++) 
+		{
+			cin >> a[i][j];
+		}
+	}
+}
+
+void saddle::saddle_point()
+    {
+        cout<<"\nSaddle Points are ";
+         for(int i=0;i<r;i++)
+             {
+                int min=a[i][0];
+                int max=a[i][0];
+                int min_index=0;
+                int max_index=0;
+                bool min_ans=true;
+                bool max_ans=true;
+            
+                for(int j=1;j<c;j++)
+                    {
+                        if(a[i][j]<min)
+                        {
+                            min=a[i][j];
+                            min_index=j;
+                        }
+                
+                        if(a[i][j]>max)
+                        {
+                            max=a[i][j];
+                            max_index=j;
+                        }
+                    }   
+            
+            
+                for(int j=0;j<r;j++)
+                    {
+                        if(a[j][min_index]>min)
+                        {
+                            min_ans=false;
+                        }
+                
+                        if(a[j][max_index]<max)
+                        {
+                            max_ans=false;
+                        }
+                
+                        if(min_ans==false && max_ans==false)
+                        {
+                            break;
+                        }
+                    }
+            
+                    if(min_ans)
+                    cout<<a[i][min_index]<<" ";
+                    else if(max_ans)
+                    cout<<a[i][max_index]<<" ";
+                    else
+                    continue;
+            
             }
-        }
-        
-        for(int i=0;i<r;i++)
-        {
-            int min=a[i][0];
-            int max=a[i][0];
-            int min_index=0;
-            int max_index=0;
-            bool min_ans=true;
-            bool max_ans=true;
-            
-            for(int j=1;j<c;j++)
-            {
-                if(a[i][j]<min)
-                {
-                    min=a[i][j];
-                    min_index=j;
-                }
-                
-                if(a[i][j]>max)
-                {
-                    max=a[i][j];
-                    max_index=j;
-                }
-                
-            }
-            
-            
-            for(int j=0;j<r;j++)
-            {
-                if(a[j][min_index]>min)
-                {
-                    min_ans=false;
-                }
-                
-                if(a[j][max_index]<max)
-                {
-                    max_ans=false;
-                }
-                
-                if(min_ans==false && max_ans==false)
-                {
-                    break;
-                }
-            }
-            
-            if(min_ans)
-            cout<<a[i][min_index]<<" ";
-            else if(max_ans)
-            cout<<a[i][max_index]<<" ";
-            else
-            continue;
-            
-        }
-        return 0;
     }
+
+
+int main()
+{
+    saddle obj;
+    obj.get_matrix();
+    obj.saddle_point();
+    return 0;
+}
