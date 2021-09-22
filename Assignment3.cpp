5c36a5de8f57baf1ef855b75b6fadd90d91c9773
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2021-09-22 10:26:56 +0530

    Update Assignment3.cpp

--- Assignment3.cpp
+++ Assignment3.cpp
@@ -1,8 +1,27 @@
-    #include<iostream>
-    using namespace std;
-    int main()
-    {
-        char s[]={'1','3','a','s','t','i'};
+/* 
+ 
+    A set S=(1,3, a, s, t, i} represent alphanumeric characters. Write a program in C++ to generate all possible passwords
+    of length 4.
+    We can form n^4 passwords, with repetition. (here, n=6  So, 6^4 = 1296)
+    But without REPETITION, we get No of possible outcomes = 360
+*/
+
+
+
+#include<iostream>
+using namespace std;
+#define c 6
+
+class password
+{
+    public:
+    char s[c];
+    void pass();
+};
+
+void password::pass()
+{
+    char s[]={'1','3','a','s','t','i'};
         int count=0;
         for(int i=0;i<6;i++)
         {
@@ -41,5 +60,17 @@
             }
         }
         cout<<"Total Number of Possible Passwords without repetition are "<<count;
-        return 0;
-    }
+}
+
+
+
+int main()
+{
+    password obj;
+    obj.pass();
+    return 0;
+}
+
+
+
+
