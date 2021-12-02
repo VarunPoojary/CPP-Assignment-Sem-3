294c9bdbea55a0913984ce9caf8f38d73b615b17
Varun Poojary <74899684+VarunPoojary@users.noreply.github.com>
2021-12-02 13:39:49 +0530

    Update Assignment11.cpp

--- Assignment11.cpp
+++ Assignment11.cpp
@@ -1,3 +1,9 @@
+/*
+Write a CPP program to store first year percentage of students in array. Write function for sorting array of floating point numbers in ascending order using
+a) Selection Sort
+b) Bubble sort and display top five scores.
+*/
+
 #include <iostream>
 using namespace std;
 # define size 10
